ba02ab80fcbf1d4fe4a377274caf783595db2ebf
Dylan Lukes <lukes.dylan@gmail.com>
2014-07-23 13:34:55 -0400

    coffee -> iced

--- index.js
+++ index.js
@@ -2,30 +2,30 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-var coffee = require("coffee-script");
+var iced = require("iced-coffee-script");
 var loaderUtils = require("loader-utils");
 module.exports = function(source) {
 	this.cacheable && this.cacheable();
-	var coffeeRequest = loaderUtils.getRemainingRequest(this);
+	var icedRequest = loaderUtils.getRemainingRequest(this);
 	var jsRequest = loaderUtils.getCurrentRequest(this);
 	var query = loaderUtils.parseQuery(this.query);
 	var result;
 	try {
-		result = coffee.compile(source, {
+		result = iced.compile(source, {
 			literate: query.literate,
-			filename: coffeeRequest,
+			filename: icedRequest,
 			debug: this.debug,
 			bare: true,
 			sourceMap: true,
 			sourceRoot: "",
-			sourceFiles: [coffeeRequest],
+			sourceFiles: [icedRequest],
 			generatedFile: jsRequest
 		});
 	} catch (e) {
 		var err = "";
 		if (e.location == null || e.location.first_column == null || e.location.first_line == null) {
-			err += "Got an unexpected exception from the coffee-script compiler. The original exception was: " + e + "\n";
-			err += "(The coffee-script compiler should not raise *unexpected* exceptions. You can file this error as an issue of the coffee-script compiler: https://github.com/jashkenas/coffee-script/issues)\n";
+			err += "Got an unexpected exception from the iced-coffee-script compiler. The original exception was: " + e + "\n";
+			err += "(The iced-coffee-script compiler should not raise *unexpected* exceptions. You can file this error as an issue of the iced-coffee-script compiler: https://github.com/maxtaco/coffee-script/issues)\n";
 		} else {
 			var codeLine = source.split("\n")[e.location.first_line];
 			var offendingCharacter = (e.location.first_column < codeLine.length) ? codeLine[e.location.first_column] : "";
